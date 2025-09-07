2a51ee499ce61268ac89ca983dcb3a9a10e0fbbf
Divya <divyabaheti101@gmail.com>
2025-09-07 20:12:13 +0530

    truffle project deployment

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,14 @@
+const Oracle = artifacts.require("Oracle");
+const Consumer = artifacts.require("Consumer");
+
+module.exports = async function (deployer, _network, _accounts) {
+    const [admin, reporter1, _] = _accounts;
+
+    // Deploy Oracle first
+    await deployer.deploy(Oracle, admin);
+    const oracle = await Oracle.deployed();
+    oracle.addReporter(reporter1, true);
+
+    // Deploy Consumer with Oracle address
+    await deployer.deploy(Consumer, oracle.address);
+};
